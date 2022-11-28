afa5edbc429736299a52e370700fc660c4207ec7
geisondossantos <geison_bf@hotmail.com>
2022-11-27 21:27:09 -0300

    First Commit

---
+++ fundamento-js-cod3r/parNomeValor.js
@@ -0,0 +1,22 @@
+// par nome/valor
+const saudacao = "opa"; //contexto léxico 1
+
+function exec() {
+  const saudacao = "falaaa"; // contexto léxico 2
+  return saudacao;
+}
+
+//Objetos são gurpos aninhados de pares nome/valor
+const Cliente = {
+  nome: "Geison",
+  idade: 34,
+  peso: 62,
+  endereco: {
+    logradouro: "Rua exemplo",
+    numero: 123,
+  },
+};
+
+console.log(saudacao);
+console.log(exec());
+console.log(Cliente);
