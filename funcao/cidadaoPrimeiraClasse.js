99f3475f8735c3fc37297c30ea7f92086e40370a
geisondossantos <geison_bf@hotmail.com>
2023-01-19 19:07:44 -0300

    tab size = 2

--- funcao/cidadaoPrimeiraClasse.js
+++ funcao/cidadaoPrimeiraClasse.js
@@ -9,35 +9,35 @@ const fun2 = function () {};
 
 // armazenar em um array
 const array = [
-    function (a, b) {
-        return a + b;
-    },
-    fun1,
-    fun2,
+  function (a, b) {
+    return a + b;
+  },
+  fun1,
+  fun2,
 ];
 console.log(array[0](1, 2));
 
 // armazenar em um atributo de objeto
 const obj = {};
 obj.falar = function () {
-    return "Opa";
+  return "Opa";
 };
 console.log(obj.falar());
 
 // passar função como param
 function run(fun) {
-    fun();
+  fun();
 }
 
 run(function () {
-    console.log("Executando...");
+  console.log("Executando...");
 });
 
 // Uma função pode retornar/conter uma função
 function soma(a, b) {
-    return function (c) {
-        console.log(a + b + c);
-    };
+  return function (c) {
+    console.log(a + b + c);
+  };
 }
 
 soma(2, 3)(4);
